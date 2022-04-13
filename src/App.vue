9714c59c0a339a1b846e91cbb13ab829bc4b77f0
nik.mironov <nik.mironov.programmer@gmail.com>
2022-04-13 17:19:16 +0300

    header component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="container">
+      <div class="container__header">
+        <HeaderComponent />
+      </div>
+      <div class="container__main"></div>
+      <div class="container__"></div>
+    </div>
+  </div>
 </template>
 
 <script>
+import HeaderComponent from "./components/default/header-component.vue";
 export default {
+  components: { HeaderComponent },
   name: "App",
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.container {
+  margin: 0 auto;
+  max-width: 1200px;
+}
+</style>
---
+++ src/components/default/header-component.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="header">
+    <div class="header__logo">
+      <img src="../../assets/img/video_logo.png" alt="" />
+      <h1 class="title">{{ title }}</h1>
+    </div>
+    <div class="header__search">
+      <input
+        class="input__search-video"
+        placeholder="Введите название для поиска"
+        type="search"
+        name="input_search"
+      />
+    </div>
+    <div class="header__controls">
+      <button class="buttons">
+        <img src="../../assets/img/contacts.png" title="Контакты" alt="img" />
+      </button>
+      <button class="buttons">
+        <img src="../../assets/img/support.png" alt="img" title="Поддержка" />
+      </button>
+      <button class="buttons">
+        <img src="../../assets/img/bell.png" alt="img" title="Уведомления" />
+      </button>
+      <button class="buttons">
+        <img src="../../assets/img/loginOut.png" alt="img" title="Вход/Выход" />
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "header-component",
+  data() {
+    return {
+      title: "RUMEDIA",
+    };
+  },
+};
+</script>
+<style lang="scss">
+.header {
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  flex-basis: 33.334%;
+
+  .header__logo {
+    display: flex;
+    align-items: center;
+    .title {
+      margin-left: 5px;
+      color: rgb(12, 116, 235);
+    }
+  }
+  .header__search {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .input__search-video {
+      width: 500px;
+      height: 30px;
+      padding: 5px;
+      border: none;
+      box-shadow: 1px 1px 1px 1px grey;
+      border-radius: 5px;
+    }
+  }
+  .header__controls {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .buttons {
+      margin-right: 5px;
+      padding: 2px;
+      border: none;
+      cursor: pointer;
+      background-color: white;
+    }
+    button:last-child {
+      margin-left: 20px;
+    }
+  }
+}
+</style>
